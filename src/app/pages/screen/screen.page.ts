0ecd925b25e50271ad8187816905cac9e00dda76
CatrielAlfonso <catrielalfonso77@gmail.com>
2025-09-12 23:02:53 -0300

    Se actualiza screen

---
+++ src/app/pages/screen/screen.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-screen',
+  templateUrl: './screen.page.html',
+  styleUrls: ['./screen.page.scss'],
+  imports: [IonContent]
+})
+export class ScreenPage implements OnInit {
+
+    constructor(private router: Router) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigateByUrl('/home');
+    }, 3800);
+  }
+
+}
