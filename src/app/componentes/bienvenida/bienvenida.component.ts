c06251182b179757de0c3f28754bc25dfeb33fae
CatrielAlfonso <catrielalfonso77@gmail.com>
2025-09-25 01:44:36 -0300

    punto 4 registro de mesa y componente bienvenida

---
+++ src/app/componentes/bienvenida/bienvenida.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router,RouterLink } from '@angular/router';
+import { IonContent,IonButton, IonIcon} from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-bienvenida',
+  templateUrl: './bienvenida.component.html',
+  styleUrls: ['./bienvenida.component.scss'],
+  imports: [IonContent, IonButton,IonIcon, CommonModule, RouterLink]
+})
+export class BienvenidaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
