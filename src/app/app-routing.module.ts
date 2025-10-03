80634dff0a0737965fb19a34716d886258cb61c0
CatrielAlfonso <catrielalfonso77@gmail.com>
2025-10-03 20:51:13 -0300

    Se fusionan cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -88,7 +88,16 @@ const routes: Routes = [
     path: 'aprobacion-clientes',
     loadComponent: () => import('./componentes/aprobacion-clientes/aprobacion-clientes.component').then(m => m.AprobacionClientesComponent)
   },
+  {
+    path: 'carrito',
+    loadComponent: () => import('./componentes/carrito/carrito.component').then(m => m.CarritoComponent)
+  },
 
+  {
+    path: 'maitre',
+    loadComponent: () => import('./componentes/maitre-mesas/maitre-mesas.component').then(m => m.MaitreMesasComponent)
+  },
+  
  
 ];
 
