144b16b4dcf3de3c90ef26f091f6bffd74322e82
LucasCarmagnola <carmagnola.lucas@gmail.com>
2025-10-04 13:41:45 -0300

    agregue uid en las tablas, y sigo modificando el carrito de compras/pedidomozo

---
+++ src/app/componentes/pedidos-mozo/pedidos-mozo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-mozo',
+  templateUrl: './pedidos-mozo.component.html',
+  styleUrls: ['./pedidos-mozo.component.scss'],
+})
+export class PedidosMozoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
