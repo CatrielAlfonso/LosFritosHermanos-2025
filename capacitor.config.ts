171c134186b38d5429e4f2dda198668dcb2a690a
Behrens0 <tomasbehrens0@gmail.com>
2025-11-29 02:12:34 -0300

    splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,18 +4,29 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'losFritosHermanos',
   webDir: 'www',
-   "plugins": 
-  {
+  backgroundColor: '#1942d7',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    backgroundColor: '#1942d7'
+  },
+  plugins: {
     CapacitorHttp: {
-      enabled:true
+      enabled: true
     },
-    "SplashScreen": {
-      "launchAutoHide": false, // Asegúrate de que esté en false
-       backgroundColor: '#1942d7',
+    SplashScreen: {
+      backgroundColor: '#1942d7',
+      showSpinner: false,
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: true,
+      splashImmersive: true,
+      launchShowDuration: 2000,
+      launchAutoHide: false
     },
-    "PushNotifications": {
-      "presentationOptions": ["badge", "sound", "alert"]
-    }          
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert']
+    }
   }
 };
 
